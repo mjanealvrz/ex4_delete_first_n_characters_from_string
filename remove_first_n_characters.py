1a7cbabc232f6c7666b294cb23e0ca703e0fa594
MJ Gillado <mjgillado0@gmail.com>
2024-01-23 00:28:57 +0800

    Added pseudocode

--- remove_first_n_characters.py
+++ remove_first_n_characters.py
@@ -1,2 +1,17 @@
-# Write a program to remove characters from a string starting from zero up to n and return a new string.
+# Write a program to remove characters from a string 
+#starting from zero up to n and return a new string.
 
+# Pseudocode
+
+# Ask user to input song title
+
+# Ask user the numbers to be removed
+
+
+# Create function to execute the removing of characters
+
+
+# Call the function and store result
+
+
+# Print the new song title
